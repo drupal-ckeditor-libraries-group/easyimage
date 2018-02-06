ckeditor/ckeditor4

Reuse Cloud Services loader for Easy Image.

plugins/easyimage/plugin.js
@@ -138,6 +138,8 @@
 
 			loadMethod: 'loadAndUpload',
 
+			loaderType: CKEDITOR.plugins.cloudservices.fileLoader,
+
 			additionalRequestParameters: {
 				isEasyImage: true
 			},
@@ -163,86 +165,6 @@
 				this.replaceWith( '<img src="' + upload.responseData.response[ 'default' ] + '" srcset="' +
 					srcset + '" sizes="100vw">' );
 			}
-		} );
-
-		editor.on( 'fileUploadRequest', function( evt ) {
-			var requestData = evt.data.requestData;
-
-			if ( !requestData.isEasyImage ) {
-				return;
-			}
-
-			evt.data.requestData.file = evt.data.requestData.upload;
-			delete evt.data.requestData.upload;
-
-			// This property is used by fileUploadResponse callback to identify EI requests.
-			evt.data.fileLoader.isEasyImage = true;
-
-			evt.data.fileLoader.xhr.setRequestHeader( 'Authorization', editor.config.easyimage_token );
-		} );
-
-		editor.on( 'fileUploadResponse', function( evt ) {
-			var fileLoader = evt.data.fileLoader,
-				xhr = fileLoader.xhr,
-				response;
-
-			if ( !fileLoader.isEasyImage ) {
-				return;
-			}
-
-			evt.stop();
-
-			try {
-				response = JSON.parse( xhr.responseText );
-
-				evt.data.response = response;
-			} catch ( e ) {
-				CKEDITOR.warn( 'filetools-response-error', { responseText: xhr.responseText } );
-			}
-		} );
-
-		// Handle images which are not available in the dataTransfer.
-		// This means that we need to read them from the <img src="data:..."> elements.
-		editor.on( 'paste', function( evt ) {
-			var fileTools = CKEDITOR.fileTools;
-
-			// For performance reason do not parse data if it does not contain img tag and data attribute.
-			if ( !evt.data.dataValue.match( /<img[\s\S]+data:/i ) ) {
-				return;
-			}
-
-			var data = evt.data,
-				// Prevent XSS attacks.
-				tempDoc = document.implementation.createHTMLDocument( '' ),
-				temp = new CKEDITOR.dom.element( tempDoc.body ),
-				imgs, img, i;
-
-			// Without this isReadOnly will not works properly.
-			temp.data( 'cke-editable', 1 );
-
-			temp.appendHtml( data.dataValue );
-
-			imgs = temp.find( 'img' );
-
-			for ( i = 0; i < imgs.count(); i++ ) {
-				img = imgs.getItem( i );
-
-				// Image have to contain src=data:...
-				var isDataInSrc = img.getAttribute( 'src' ) && img.getAttribute( 'src' ).substring( 0, 5 ) == 'data:',
-					isRealObject = img.data( 'cke-realelement' ) === null;
-
-				// We are not uploading images in non-editable blocs and fake objects (http://dev.ckeditor.com/ticket/13003).
-				if ( isDataInSrc && isRealObject && !img.data( 'cke-upload-id' ) && !img.isReadOnly( 1 ) ) {
-					var loader = editor.uploadRepository.create( img.getAttribute( 'src' ) );
-					loader.upload( uploadUrl );
-
-					fileTools.markElement( img, 'uploadeasyimage', loader.id );
-
-					fileTools.bindNotifications( editor, loader );
-				}
-			}
-
-			data.dataValue = temp.getHtml();
 		} );
 	}
 
@@ -290,7 +212,7 @@
 	};
 
 	CKEDITOR.plugins.add( 'easyimage', {
-		requires: 'image2,uploadwidget,contextmenu,dialog',
+		requires: 'image2,uploadwidget,contextmenu,dialog,cloudservices',
 		lang: 'en',
 
 		onLoad: function() {

drupal-ckeditor-libraries-group/easyimage

plugin.js
@@ -150,6 +150,8 @@
 
 			loadMethod: 'loadAndUpload',
 
+			loaderType: CKEDITOR.plugins.cloudservices.fileLoader,
+
 			additionalRequestParameters: {
 				isEasyImage: true
 			},
@@ -175,86 +177,6 @@
 				this.replaceWith( '<img src="' + upload.responseData.response[ 'default' ] + '" srcset="' +
 					srcset + '" sizes="100vw">' );
 			}
-		} );
-
-		editor.on( 'fileUploadRequest', function( evt ) {
-			var requestData = evt.data.requestData;
-
-			if ( !requestData.isEasyImage ) {
-				return;
-			}
-
-			evt.data.requestData.file = evt.data.requestData.upload;
-			delete evt.data.requestData.upload;
-
-			// This property is used by fileUploadResponse callback to identify EI requests.
-			evt.data.fileLoader.isEasyImage = true;
-
-			evt.data.fileLoader.xhr.setRequestHeader( 'Authorization', editor.config.easyimage_token );
-		} );
-
-		editor.on( 'fileUploadResponse', function( evt ) {
-			var fileLoader = evt.data.fileLoader,
-				xhr = fileLoader.xhr,
-				response;
-
-			if ( !fileLoader.isEasyImage ) {
-				return;
-			}
-
-			evt.stop();
-
-			try {
-				response = JSON.parse( xhr.responseText );
-
-				evt.data.response = response;
-			} catch ( e ) {
-				CKEDITOR.warn( 'filetools-response-error', { responseText: xhr.responseText } );
-			}
-		} );
-
-		// Handle images which are not available in the dataTransfer.
-		// This means that we need to read them from the <img src="data:..."> elements.
-		editor.on( 'paste', function( evt ) {
-			var fileTools = CKEDITOR.fileTools;
-
-			// For performance reason do not parse data if it does not contain img tag and data attribute.
-			if ( !evt.data.dataValue.match( /<img[\s\S]+data:/i ) ) {
-				return;
-			}
-
-			var data = evt.data,
-				// Prevent XSS attacks.
-				tempDoc = document.implementation.createHTMLDocument( '' ),
-				temp = new CKEDITOR.dom.element( tempDoc.body ),
-				imgs, img, i;
-
-			// Without this isReadOnly will not works properly.
-			temp.data( 'cke-editable', 1 );
-
-			temp.appendHtml( data.dataValue );
-
-			imgs = temp.find( 'img' );
-
-			for ( i = 0; i < imgs.count(); i++ ) {
-				img = imgs.getItem( i );
-
-				// Image have to contain src=data:...
-				var isDataInSrc = img.getAttribute( 'src' ) && img.getAttribute( 'src' ).substring( 0, 5 ) == 'data:',
-					isRealObject = img.data( 'cke-realelement' ) === null;
-
-				// We are not uploading images in non-editable blocs and fake objects (http://dev.ckeditor.com/ticket/13003).
-				if ( isDataInSrc && isRealObject && !img.data( 'cke-upload-id' ) && !img.isReadOnly( 1 ) ) {
-					var loader = editor.uploadRepository.create( img.getAttribute( 'src' ) );
-					loader.upload( uploadUrl );
-
-					fileTools.markElement( img, 'uploadeasyimage', loader.id );
-
-					fileTools.bindNotifications( editor, loader );
-				}
-			}
-
-			data.dataValue = temp.getHtml();
 		} );
 	}
 
@@ -302,7 +224,7 @@
 	};
 
 	CKEDITOR.plugins.add( 'easyimage', {
-		requires: 'imagebase,uploadwidget,contextmenu,dialog',
+		requires: 'imagebase,uploadwidget,contextmenu,dialog,cloudservices',
 		lang: 'en',
 
 		onLoad: function() {